1fa9ba6647c60b6591ea207fcccbd9740a65629f
Nick Crain <nicholascrain@gmail.com>
2018-11-27 09:21:19 -0800

    pluralize function completed

--- basics/Main.java
+++ basics/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
 
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
     }
 
     //TODO: Write a function called pluralize that accepts a word and a number and returns a string with the word pluralized with an “s” if the number is zero, or greater than one.
     public static String pluralize(String word, int number) {
+        if (number == 0 || number > 1)
+        word = word + "s";
         return word;
     }
 
@@ -15,6 +24,6 @@ public class Main {
 
     //TODO: Write a method clock that uses Java’s built-in LocalDateTime object to constantly print out the current time to the console, second by second. The program should run until someone manually kills it with CTRL-C or presses the “stop” button in their IDE. Each time should only be printed once. Your program should detect when the seconds increase and only print something out when the timestamp changes. Zero-pad single digit numbers as strings so the timestamp is always the same number of characters.
     public static void clock() {
-        
+
     }
 }
