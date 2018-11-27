383fe82b101bf5e7c03809e203ecb3fa934e6ea9
Nick Crain <nicholascrain@gmail.com>
2018-11-27 10:12:54 -0800

    clock function completed

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Main {
@@ -14,6 +16,8 @@ public class Main {
         flipNHeads(1);
         flipNHeads(2);
         flipNHeads(3);
+
+        clock();
     }
 
     public static String pluralize(String word, int number) {
@@ -22,7 +26,6 @@ public class Main {
         return word;
     }
 
-    //TODO: Write a function called flipNHeads that accepts an integer n and flips coins until n heads are flipped in a row. Simulate coin flipping by choosing a random number between 0 and 1. Numbers below .5 are considered tails. Numbers at and above .5 are considered heads. Print out heads or tails on one line for each flip. Print It took FLIPS flips to flip N heads in a row. once n heads have been in a row.
     public static void flipNHeads(int streak) {
         int flips = 0;
         int heads = 0;
@@ -43,6 +46,17 @@ public class Main {
 
     //TODO: Write a method clock that uses Java’s built-in LocalDateTime object to constantly print out the current time to the console, second by second. The program should run until someone manually kills it with CTRL-C or presses the “stop” button in their IDE. Each time should only be printed once. Your program should detect when the seconds increase and only print something out when the timestamp changes. Zero-pad single digit numbers as strings so the timestamp is always the same number of characters.
     public static void clock() {
+        LocalDateTime now = LocalDateTime.now();
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        while (true) {
+            int second = now.getSecond();
+            do {
+                // System.out.println(second == now.getSecond());
+                now = LocalDateTime.now();
+                time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            } while (second == now.getSecond());
 
+            System.out.println(time);
+        }
     }
 }
