5efb45f99a921c821663b8c49c8505d75913279e
Nick Crain <nicholascrain@gmail.com>
2018-11-29 11:19:42 -0800

    line #s and check for ; at end

--- linter/src/main/java/App.java
+++ linter/src/main/java/App.java
@@ -11,25 +11,19 @@ public class App {
 
     public static void main(String[] args) {
         Path gates = Paths.get("resources/gates.js");
-        linter(gates);
+        jsLinter(gates);
     }
 
     /*TODO: Write a method that reads a JavaScript file with a given Path, and prints an error whenever it finds a line that doesn’t end in a semi-colon.
-    TODO Read through the file line by line. Print an error message such as "Line 3: Missing semicolon." if a line is missing a semicolon.
-    TODO Don’t show an error if the line is empty.
-    TODO Don’t show an error if the line ends with an opening curly brace {
-    TODO Don’t show an error if the line ends with an closing curly brace }
-    TODO Don’t show an error if the line contains if or else
     */
 
-    public static void linter(Path file) {
+    public static void jsLinter(Path file) {
         try(BufferedReader reader = Files.newBufferedReader(file)) {
             String currentLine = "";
-            int line = 1;
+            int line = 0;
             while (currentLine != null) {
-                //check for semi-colons based on above requirements
-                if (!currentLine.isEmpty() && !currentLine.endsWith("{") && !currentLine.endsWith("}") && !currentLine.contains("if") && !currentLine.contains("else")) {
-                    System.out.println("Line " + line + ": Missing a semicolon");
+                if (!currentLine.isEmpty() && !currentLine.endsWith("{") && !currentLine.endsWith("}") && !currentLine.contains("if") && !currentLine.contains("else") && !currentLine.endsWith(";")) {
+                    System.out.println("Line " + line + ": Missing semicolon");
                 }
 
                 line++;
