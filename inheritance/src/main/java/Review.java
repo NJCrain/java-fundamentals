c2c1eb497cb52ddcb0a40bac16729439cd90b0b8
Nick Crain <nicholascrain@gmail.com>
2018-12-04 09:20:06 -0800

    initial lab setup

---
+++ inheritance/src/main/java/Review.java
@@ -0,0 +1,6 @@
+public class Review {
+    //TODO: Each Review should have a body, an author, and a number of stars.
+    //TODO: Implement a reasonable toString method for Reviews.
+    //TODO: Test your toString method.
+    //TODO: Write a test to create an instance of Review and ensure that its toString is working properly.
+}
