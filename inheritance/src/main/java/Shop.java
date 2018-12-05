2e32840aedfe5b0a7186acfd1114e8ce6f404a13
Nick Crain <nicholascrain@gmail.com>
2018-12-05 10:01:54 -0800

    Shops can add reviews and track them

--- inheritance/src/main/java/Shop.java
+++ inheritance/src/main/java/Shop.java
@@ -1,20 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Shop implements Reviewable {
     public String name;
     public String description;
     public String price;
+    public double stars;
+    public Set<Review> reviews = new HashSet<>();
 
     public Shop(String name, String description, String price) {
         this.name = name;
         this.description = description;
         this.price = price;
+        this.stars = 0;
     }
 
     public String toString() {
-        return "Shop name: " + name + " | price: " + price + "\ndescription: " + description;
+        return "Shop name: " + name + " | stars: " + stars + " | price: " + price + "\ndescription: " + description;
     }
-    //TODO: Add tests for the basics of your Shop class.
 
     public void addReview(Review toAdd) {
+        reviews.add(toAdd);
+        toAdd.location = this;
+        double totalStars = 0;
+        for (Review review : reviews) {
+            totalStars += (double) review.stars;
+        }
+        stars = totalStars / (double) reviews.size();
+    }
 
+    public String getName() {
+        return this.name;
     }
 }
